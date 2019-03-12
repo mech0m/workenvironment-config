83f7270d39e5f0ee40164a458573af79784f8a0f
mech0m <38977579+mech0m@users.noreply.github.com>
2019-03-12 10:11:56 +0100

    Fixed BUG: wrong number of arguments in config.el

--- spacemacs/config.el
+++ spacemacs/config.el
@@ -358,8 +358,8 @@ you should place your code here."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    ))
+  
+   )
  '(safe-local-variable-values
    (quote
     ((TeX-command-extra-options . "-shell-escape -f")
